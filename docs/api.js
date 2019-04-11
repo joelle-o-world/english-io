90bdda75c9eb1cfdc300d2eb139c723788723d34
Joel Plowright <joelplow@gmail.com>
2019-04-11 15:12:47 +0100

    documentation

---
+++ docs/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DescriptionContext",
+        "Entity",
+        "FactListener",
+        "NounPhraseSentence",
+        "Predicate",
+        "PredicateSet",
+        "PredicateSyntax",
+        "Sentence",
+        "SentenceQueue",
+        "TimedPredicate",
+        "WanderingDescriber",
+        "plural"
+    ],
+    "modules": [
+        "entity-game"
+    ],
+    "allModules": [
+        {
+            "displayName": "entity-game",
+            "name": "entity-game"
+        }
+    ],
+    "elements": []
+} };
+});
